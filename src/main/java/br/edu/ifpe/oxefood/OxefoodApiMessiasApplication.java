46a2e40d52b733981634a3c78f180fed3cb0721e
Messias <elmanomessias@gmail.com>
2026-08-13 21:07:19 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ifpe/oxefood/OxefoodApiMessiasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiMessiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiMessiasApplication.class, args);
+	}
+
+}
