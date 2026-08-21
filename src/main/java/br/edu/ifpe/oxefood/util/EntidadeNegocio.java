b5b99f15cbdbfb9dfc240a1bb33758d45080e72e
Messias <elmanomessias@gmail.com>
2026-08-20 21:08:01 -0300

    segunda aula parte 2

--- src/main/java/br/edu/ifpe/oxefood/util/EntidadeNegocio.java
+++ src/main/java/br/edu/ifpe/oxefood/util/EntidadeNegocio.java
@@ -1,5 +1,12 @@
 package br.edu.ifpe.oxefood.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +14,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(of = { "id" })
-public abstract class EntidadeNegocio {
+@MappedSuperclass
 
+public abstract class EntidadeNegocio {
+// o @Id faz ele ser chave primaria, generatedvalue deve falar a estrategia pra gerar
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+      @JsonIgnore
+    @Column
 
     private Boolean habilitado;
     
